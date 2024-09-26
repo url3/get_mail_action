4b6c86c630a230aa46fd5d40aa3f9755115cd2df
url3 <76902866+url3@users.noreply.github.com>
2024-09-26 18:14:26 +0800

    Update update_index.py

--- update_index.py
+++ update_index.py
@@ -26,14 +26,14 @@ def update_index():
 
     # 清除旧的验证码内容，保留其他内容
     new_content = re.sub(r'(<p>验证码:.*?</p>\n*)*', '', content)
-    new_content = re.sub(r'(<p>获取时间:.*?</p>\n*)*', '', new_content)
+    new_content = re.sub(r'(<p>刷新时间:.*?</p>\n*)*', '', new_content)
 
     # 添加最新获取验证码时间
-    new_content += f'<p>获取时间: {current_time}</p>\n'
+    new_content += f'<p>刷新时间: {current_time}</p>\n'
 
     # 按从后往前的顺序将验证码添加到 index.html
     for code in codes:
-        new_content += f'<p>验证码: {code}</p>\n'
+        new_content += f'<p>{code}</p>\n'
 
     # 写入更新后的内容
     with open(index_path, 'w') as file:
